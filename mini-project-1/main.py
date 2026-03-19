7a69d2895eb3be1292fec613e3807c295ed5e6be
Muhammad Ayyan <muhammad.ayyan.aieng@gmail.com>
2026-03-19 21:04:45 +0200

    mini-project-1: refactor into APIRouter

--- mini-project-1/main.py
+++ mini-project-1/main.py
@@ -9,6 +9,3 @@ async def root():
     return {"message": "Welcome to the Library API"}
 
 app.include_router(book_router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
